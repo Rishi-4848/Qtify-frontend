784773f9c266c8c5cfe792a50a1bd8ff81182915
Rishi-4848 <117424531+Rishi-4848@users.noreply.github.com>
2023-11-02 14:07:26 +0530

    “cardCarouselTopAlbums-qtify-takehome”

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "./NavBar.module.css"
 import Button from '../Button/Button';
 import Search from '../Search/Search';
-import Logo from '../Logo/Logo';
+import Logo from '../Images/Logo/Logo';
 
 
 const NavBar = () => {
